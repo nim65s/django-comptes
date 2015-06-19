441aa3107a2995b6e0b640f7ba681fad9a033810
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-06-19 18:37:08 +0200

    migration

---
+++ migrations/0003_ordre.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('comptes', '0002_auto_20150517_2022'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='dette',
+            options={'ordering': ['-moment']},
+        ),
+        migrations.AlterModelOptions(
+            name='remboursement',
+            options={'ordering': ['-moment']},
+        ),
+    ]
