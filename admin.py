f8696116da61fc4b997e0803103744e7878cd14f
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-10-08 19:25:43 +0200

    Si une occasion n’a pas de membre (ie. a *tous* les membres), vaut mieux pas les filtrer

--- admin.py
+++ admin.py
@@ -8,12 +8,12 @@ class DetteAdmin(ModelAdmin):
     radio_fields = {"occasion": HORIZONTAL}
 
     def formfield_for_manytomany(self, db_field, request, **kwargs):
-        if db_field.name == "debiteurs":
+        if db_field.name == "debiteurs" and not Occasion.objects.filter(membres__isnull=True).exists():
             kwargs['queryset'] = User.objects.filter(occasion__clos=False).distinct()
         return super(DetteAdmin, self).formfield_for_manytomany(db_field, request, **kwargs)
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == "creancier":
+        if db_field.name == "creancier" and not Occasion.objects.filter(membres__isnull=True).exists():
             kwargs['queryset'] = User.objects.filter(occasion__clos=False).distinct()
         if db_field.name == "occasion":
             kwargs['queryset'] = Occasion.objects.filter(clos=False)
@@ -26,7 +26,7 @@ class RemboursementAdmin(ModelAdmin):
     radio_fields = {"occasion": HORIZONTAL}
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name in ["crediteur", "credite"]:
+        if db_field.name in ["crediteur", "credite"] and not Occasion.objects.filter(membres__isnull=True).exists():
             kwargs['queryset'] = User.objects.filter(occasion__clos=False).distinct()
         if db_field.name == "occasion":
             kwargs['queryset'] = Occasion.objects.filter(clos=False)
