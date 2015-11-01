ed1291d760968a255a4d108a42ee34e510e96464
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-11-01 13:25:04 +0100

    on rend le tout usable…

--- urls.py
+++ urls.py
@@ -5,5 +5,5 @@ from .views import DetteCreateView, home
 urlpatterns = patterns('',
     url(r'^$', home, name='home'),
     url(r'^(?P<slug>[^/]+)$', home, name='occasion'),
-    url(r'^(?P<oc_slug>[^/]+)/dette$', DetteCreateView.as_view(), name='add_dette'),
+    url(r'^(?P<oc_slug>[^/]+)/dette$', DetteCreateView.as_view(), name='dette'),
 )
