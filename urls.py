3513fbad939d74426d3c24447558036bba27cb11
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-12-21 12:49:41 +0100

    urls 1.8

--- urls.py
+++ urls.py
@@ -1,10 +1,11 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from .views import DetteCreateView, RemboursementCreateView, home
 
-urlpatterns = patterns('',
-    url(r'^$', home, name='home'),
-    url(r'^(?P<slug>[^/]+)$', home, name='occasion'),
-    url(r'^(?P<oc_slug>[^/]+)/dette$', DetteCreateView.as_view(), name='dette'),
-    url(r'^(?P<oc_slug>[^/]+)/remboursement$', RemboursementCreateView.as_view(), name='remboursement'),
-)
+app_name = 'comptes'
+urlpatterns = [
+        url(r'^$', home, name='home'),
+        url(r'^(?P<slug>[^/]+)$', home, name='occasion'),
+        url(r'^(?P<oc_slug>[^/]+)/dette$', DetteCreateView.as_view(), name='dette'),
+        url(r'^(?P<oc_slug>[^/]+)/remboursement$', RemboursementCreateView.as_view(), name='remboursement'),
+        ]
