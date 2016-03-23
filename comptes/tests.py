5a147426d95898996acb78ca261f54153dd20d4f
Guilhem Saurel <guilhem.saurel@gmail.com>
2016-03-24 00:54:54 +0100

    Tests

--- comptes/tests.py
+++ comptes/tests.py
@@ -1,25 +1,93 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
 
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.utils.timezone import now
 
 from .models import Dette, Occasion, Remboursement
 
+ROOT_URL = 'comptes'
 
-class OccasionTests(TestCase):
+
+class ComptesTests(TestCase):
     def setUp(self):
-        a = User.objects.create_user('a', 'a@example.org')
-        b = User.objects.create_user('b', 'b@example.org')
-        c = User.objects.create_user('c', 'c@example.org')
-        o = Occasion(nom='O', slug='o', description='test occasion 1', debut=now(), fin=now() + timedelta(days=30))
+        dt = now()
+        a, b, c = (User.objects.create_user(guy, email='%s@example.org' % guy, password=guy) for guy in 'abc')
+        o = Occasion(nom='O', slug='o', description='test occasion 1', debut=dt, fin=dt + timedelta(days=30))
+        p = Occasion(nom='P', slug='p', description='test occasion 2', debut=dt, fin=dt + timedelta(days=30))
         o.save()
-        p = Occasion(nom='P', slug='p', description='test occasion 2', debut=now(), fin=now() + timedelta(days=30))
         p.save()
         p.membres.add(a)
         p.membres.add(b)
+        d = Dette(creancier=a, montant=9, date=dt, occasion=o, scribe=a, description='Lorem Ipsum')
+        d.save()
+        d.debiteurs.add(a)
+        d.debiteurs.add(b)
+        d.debiteurs.add(c)
+        Remboursement(crediteur=b, credite=a, montant=3, date=dt.date(), time=dt.time(), scribe=c, occasion=o).save()
 
-    def test_str(self):
-        o, p = Occasion.objects.order_by('id').all()
+    # MODELS
+
+    def test_get_absolute_url(self):
+        o = Occasion.objects.first()
+        d = Dette.objects.first()
+        r = Remboursement.objects.first()
+        self.assertEqual(o.get_absolute_url(), '/%s/%s' % (ROOT_URL, o.slug))
+        self.assertEqual(d.get_absolute_url(), '/%s/%s' % (ROOT_URL, o.slug))
+        self.assertEqual(r.get_absolute_url(), '/%s/%s' % (ROOT_URL, o.slug))
+
+    # Occasion
+
+    def test_occasion_str(self):
+        o, p = Occasion.objects.all()
         self.assertEqual(str(o), 'Occasion: O')
         self.assertEqual(str(p), 'Occasion: P')
+
+    def test_occasion_get_membres(self):
+        for occasion in Occasion.objects.all():
+            for user in User.objects.all():
+                if user.username == 'c' and occasion.nom == 'P':
+                    self.assertNotIn(user, occasion.get_membres())
+                else:
+                    self.assertIn(user, occasion.get_membres())
+
+    def test_occasion_solde_des_membres(self):
+        a, b, c = User.objects.all()
+        self.assertEqual(Occasion.objects.first().solde_des_membres(), [(3, a), (0, b), (-3, c)])
+
+    def test_occasion_depenses(self):
+        self.assertEqual(Occasion.objects.first().depenses(), 9)
+
+    # Dette
+
+    def test_dette_str(self):
+        self.assertEqual(str(Dette.objects.first()), 'Dette: a a payé 9.00 à 3 personnes pour «Lorem Ipsum»')
+
+    def test_dette_debiteurs_list(self):
+        self.assertEqual(Dette.objects.first().debiteurs_list(), 'a, b, c')
+
+    # Remboursement
+
+    def test_remboursement_str(self):
+        self.assertEqual(str(Remboursement.objects.first()), 'b a remboursé 3.00 € à a')
+
+    # VIEWS
+
+    def test_access_home(self):
+        self.assertEqual(self.client.get(reverse('comptes:home')).status_code, 302)
+        self.client.login(username='a', password='a')
+        self.assertEqual(self.client.get(reverse('comptes:home')).status_code, 200)
+
+    def test_access_occasion(self):
+        self.assertEqual(self.client.get(reverse('comptes:occasion', kwargs={'slug': 'p'})).status_code, 302)
+        self.client.login(username='a', password='a')
+        self.assertEqual(self.client.get(reverse('comptes:occasion', kwargs={'slug': 'p'})).status_code, 200)
+        self.client.login(username='c', password='c')
+        self.assertEqual(self.client.get(reverse('comptes:occasion', kwargs={'slug': 'p'})).status_code, 403)
+
+    def test_access_dette(self):
+        self.client.login(username='a', password='a')
+        self.assertEqual(self.client.get(reverse('comptes:dette', kwargs={'oc_slug': 'p'})).status_code, 200)
+        self.client.login(username='c', password='c')
+        self.assertEqual(self.client.get(reverse('comptes:dette', kwargs={'oc_slug': 'p'})).status_code, 302)
--- project_for_tests/project_for_tests/settings.py
+++ project_for_tests/project_for_tests/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'project_for_tests',
     'comptes',
+    'bootstrap3',
 ]
 
 MIDDLEWARE_CLASSES = [
