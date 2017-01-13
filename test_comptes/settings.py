2e5a4ad0355e083b59f33fa66048b1944cfb39d7
Guilhem Saurel <guilhem.saurel@gmail.com>
2017-01-13 01:41:53 +0100

    add Site framowork for travis

--- test_comptes/settings.py
+++ test_comptes/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
     'django.contrib.staticfiles',
     'test_comptes',
     'comptes',
@@ -122,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+SITE_ID = 1
