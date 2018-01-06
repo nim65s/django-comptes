06438c88f64a545fdd71d36135c8ce76b9786aca
Guilhem Saurel <guilhem.saurel@gmail.com>
2018-01-06 22:24:35 +0100

    use django.urls.path

--- comptes/urls.py
+++ comptes/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import url
+from django.urls import path
 
 from .views import DetteCreateView, RemboursementCreateView, home
 
 app_name = 'comptes'
 urlpatterns = [
-    url(r'^$', home, name='home'),
-    url(r'^(?P<slug>[^/]+)$', home, name='occasion'),
-    url(r'^(?P<oc_slug>[^/]+)/dette$', DetteCreateView.as_view(), name='dette'),
-    url(r'^(?P<oc_slug>[^/]+)/remboursement$', RemboursementCreateView.as_view(), name='remboursement'),
+    path('', home, name='home'),
+    path('<str:slug>', home, name='occasion'),
+    path('<str:oc_slug>/dette', DetteCreateView.as_view(), name='dette'),
+    path('<str:oc_slug>/remboursement', RemboursementCreateView.as_view(), name='remboursement'),
 ]
