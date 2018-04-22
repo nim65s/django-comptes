594aedaeae2e7f79ca6b8c788aace3987de832fd
Guilhem Saurel <guilhem.saurel@gmail.com>
2018-04-23 00:41:11 +0200

    Release v1.1.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-comptes',
-    version='1.0.2',
+    version='1.1.0',
     packages=['comptes'],
     install_requires=REQUIREMENTS,
     include_package_data=True,
