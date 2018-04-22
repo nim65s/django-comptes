8f194d934d928f95f42580ca8f78b212e534b7d6
Guilhem Saurel <guilhem.saurel@gmail.com>
2018-04-23 00:35:25 +0200

    mv test_comptes testproject

--- testproject/wsgi.py
+++ testproject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for test_comptes project.
+WSGI config for testproject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_comptes.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testproject.settings")
 
 application = get_wsgi_application()
