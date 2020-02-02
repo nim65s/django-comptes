5cbf6ff01aa37845f3d02744c9e956321ee21bd2
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-02-02 10:57:36 +0100

    setuptools-scm

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@
 
 import os
 import re
-from subprocess import check_output
 
 from setuptools import setup  # type: ignore
 
@@ -12,16 +11,13 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 
 with open(os.path.join(os.path.dirname(__file__), 'Pipfile')) as pipfile:
     content = pipfile.read()
-    REQUIREMENTS = re.findall(r'''\\n *['"]?([\w-]*)['"]? *=''', content.split('packages]')[1])
-
-VERSION = [tag for tag in check_output(['git', 'tag', '-l']).decode().split() if tag.startswith('v')][-1][1:]
+    REQUIREMENTS = re.findall(r'''\n *['"]?([\w-]*)['"]? *=''', content.split('packages]')[1])
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-comptes',
-    version=VERSION,
     packages=['comptes'],
     install_requires=REQUIREMENTS,
     include_package_data=True,
@@ -31,8 +27,11 @@ setup(
     long_description_content_type="text/markdown",
     url='https://github.com/nim65s/django-comptes',
     author='Guilhem Saurel',
-    author_email='webmaster@saurel.me',
+    author_email='guilhem.saurel@laas.fr',
     python_requires='>=3.6',
+    zip_safe=False,
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
@@ -42,6 +41,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
