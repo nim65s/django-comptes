e0fe1eea6b2e7e42542960b7e220e8965990b139
Guilhem Saurel <guilhem.saurel@laas.fr>
2021-08-09 23:16:32 +0200

    fix pydocstyle

--- comptes/apps.py
+++ comptes/apps.py
@@ -1,6 +1,8 @@
+"""Application configuration for comptes."""
 from django.apps import AppConfig
 
 
 class ComptesConfig(AppConfig):
+    """Application configuration for comptes."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'comptes'
